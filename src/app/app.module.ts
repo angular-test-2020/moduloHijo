bfab1e706b405d8f6e6b9ecd4c6b1e9bee74aef7
fsanchez-brujula <fsanchez@brujula.es>
2020-05-05 13:43:05 +0200

    nuevo componente list routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { MyLibModule } from 'prueba-concepto-libreria';
+import { ListRoutesComponent } from './list-routes/list-routes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ListRoutesComponent,
   ],
   imports: [
     BrowserModule,
